f3925e5c5317f515794810100dbf5d52011cb0c4
AndriyIzmaylov <82765435+AndriyIzmaylov@users.noreply.github.com>
2022-11-22 21:54:03 +0200

    Add files via upload

--- features UI/environment.py
+++ features UI/environment.py
@@ -1,8 +1,11 @@
 import json
+import allure
+import time
 from pathlib import Path
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
+from allure_commons.types import AttachmentType
 
 project_path = Path.cwd()
 file_path = project_path.joinpath("creds.json")
@@ -13,7 +16,18 @@ def before_all(context):
         login_variables = json.load(f)
         context.login_variables = login_variables
 
-    context.driver = webdriver.Chrome(ChromeDriverManager().install())
+    port = 4488
+
+    # Chrome options
+    options = webdriver.ChromeOptions()
+    options.add_argument('--ignore-ssl-errors=yes')
+    options.add_argument('--ignore-certificate-errors')
+
+    # Run Chrome with options
+    context.driver = webdriver.Remote(
+        command_executor=f'http://localhost:{port}/wd/hub',
+        options=options
+    )
 
 
 def before_scenario(context, scenario):
@@ -29,8 +43,16 @@ def before_scenario(context, scenario):
 def after_all(context):
     context.driver.close()
 
+
 def after_scenario(context, scenario):
     context.driver.get(context.user_id_after_create)
     context.driver.find_element(By.XPATH, '//*[@id="content"]/div[1]/button').click()
     context.driver.find_element(By.XPATH, '//*[@id="deleteModal"]/div/div/div[2]/form/button').submit()
     context.driver.get("https://www.aqa.science/api-auth/logout/?next=/users/")
+
+
+def after_step(context,step):
+    if step.status == 'failed':
+        allure.attach(context.driver.get_screenshot_as_png(), name=f"Screenshot_of_failed_test_{context.scenario}",
+                      attachment_type=AttachmentType.PNG)
+        time.sleep(3)
